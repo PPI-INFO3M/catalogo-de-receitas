fcd438d7c01137c9fdf9f787b687cc065b7c23b3
João Felipe Martins Teodoro <joao.teodoro@academico.ifrn.edu.br>
2026-08-28 09:43:05 -0300

    feat: inicio da estilização

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,7 +1,16 @@
+import './Cabecalho.css'
+
 function Cabecalho() {
     return (
         <header>
-            <h1>Catálogo de Receitas</h1>
+            <h1 className='logo'>Sabor & Afetyo</h1>
+            <nav>
+                <ul>
+                    <li>Receitas</li>
+                    <li>Categorais</li>
+                    <li>Sobre</li>
+                </ul>
+            </nav>
             <p>Seu livro de receitas em um único lugar!</p>
         </header>
     );
